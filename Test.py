7da138228111d2826610f5349278a2ca79940db4
Sam Adegun <adegunsamuel@yahoo.com>
2021-03-22 23:20:35 -0500

    https://github.com/adegunsamuel/ReSkill/blob/main/ReSkill_Project

---
+++ Test.py
@@ -0,0 +1,17 @@
+base = (2)
+exponent = (38)
+print('Exponential Value is', base ** exponent)
+
+
+msg = 'Hello World'
+print(msg)
+msg.lower()
+
+
+name = input('What is your name?')
+print('\nhello')
+# print(name)
+print('Nice to meet you \nThank you')
+print(msg.upper())
+print(msg.count(''))
+print()
